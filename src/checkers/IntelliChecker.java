7ee0daeb64d6f63b75c20a107b15cb628fa64f80
Jess Valdez <jess.valdez@gmail.com>
2018-01-29 10:01:03 -0800

    first commit from JV

---
+++ src/checkers/IntelliChecker.java
@@ -0,0 +1,12 @@
+package checkers;
+
+public class IntelliChecker {
+    public static void main(String args[]) throws Exception {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+                public void run() {
+                   new CheckerFrame();
+            	}
+        });
+    }
+}
+
