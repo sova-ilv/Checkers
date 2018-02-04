df4b2875cebaf471111d329298316bc99701ae58
WORK-MACHINE\Brian <philmcelroy@vfp.us>
2018-02-03 20:42:08 -0600

    This gets the game working with images and sounds.  I needed to move the images folder and the sounds folder to the src folder.

--- src/checkers/StartPanel.java
+++ src/checkers/StartPanel.java
@@ -43,7 +43,7 @@ public class StartPanel extends JPanel{
         g.drawString("K.C.B. GAJASINGHE",180,240);
         g.drawString("070137M",320,240);
         ////////////////////////////////////////////////////////////////////////
-        g.drawImage(new ImageIcon(getClass().getResource("/checkersIcon.jpg")).getImage(),350,280,this);//checkersIcon.jpg
+        g.drawImage(new ImageIcon(getClass().getResource("/images/checkersIcon.jpg")).getImage(),350,280,this);//checkersIcon.jpg
 
 
         g.setColor(new Color(0,0,0));
